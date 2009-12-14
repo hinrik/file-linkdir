25db4a5a876e0e263c791dfa70e9e7267a0e5360
Hinrik Örn Sigurðsson <hinrik.sig@gmail.com>
2009-12-14 10:25:27 +0000

    Bump version to 0.04

--- lib/File/LinkDir.pm
+++ lib/File/LinkDir.pm
@@ -11,7 +11,7 @@ use Getopt::Long qw<:config auto_help bundling>;
 use Pod::Find qw<pod_where>;
 use Pod::Usage;
 
-our $VERSION = '0.03';
+our $VERSION = '0.04';
 my ($dry_run, $source, $dest, $recursive, $force, @add_ignore);
 my $ignore = '(?:.*/)?.(?:git|svn)(?:/.*)?$';
 
